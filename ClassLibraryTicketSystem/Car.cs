cd70b00d12a7c95b286bca35659521343297c54b
sarahBæk2021 <sbaek18@gmail.com>
2021-09-22 14:47:59 +0200

    Task 2 Implement a class “Car”, which has the following properties ● public string Licenseplate ● public DateTime Date and the following methods ● public double Price() The price is fixed at 240 which must be returned ● public string VehicleType() which returns the string "Car" Add a test project and test the two methods. Note: Remember to give the test method a descriptive name Note2: Did you remember to document your class and methods

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+    public class Car
+    {
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public string LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        ///Method  
+       /// public double Price() The price is fixed at 240 which must be returned
+        /// </summary>
+        /// <returns>240.00</returns>
+        public double Price()
+        {
+            return 240.00;
+        }
+
+        /// <summary>
+        /// Method
+        /// public string VehicleType() which returns the string "Car" 
+        /// </summary>
+        /// <returns>"Car"</returns>
+        public string VehicleType(string Car)
+        {
+            return "Car";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        /// <summary>
+        /// Testing the Price method. It should retun a double 240.00.
+        /// </summary>
+        [TestMethod()]
+        public void PriceTest_CorrectPrice_Pass()
+        {
+            //Arrange
+            double Expected = 240.00;
+            //Act
+            Car car1 = new Car();
+            double Actual = car1.Price();
+            //Assert
+            Assert.AreEqual(Expected, Actual);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //Arrange
+            string Expected = "Car";
+            Car car1 = new Car();
+            //Act
+           string Actual =  car1.VehicleType("Car");
+            //Assert
+            Assert.AreEqual(Expected, Actual);
+            //Assert.Fail();
+        }
+    }
+}
